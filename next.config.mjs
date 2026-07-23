f3378064fd3ae46c7ce5b4ae60f9c93c6dfc21ca
shankar7055 <shankarpratap220@gmail.com>
2026-07-23 15:56:12 +0530

    fix: remove unused MDX deps from next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,39 +1,15 @@
-import createMDX from '@next/mdx'
-import rehypePrettyCode from "rehype-pretty-code";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure `pageExtensions` to include markdown and MDX files
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-  // Optionally, add any other Next.js config below
   images: {
     qualities: [95, 100],
-    remotePatterns: [{
-      protocol: 'https',
-      hostname: 'api.microlink.io', // Microlink Image Preview
-      pathname: '**',
-
-    },]
-  }
-}
-
-/** @type {import('rehype-pretty-code').Options} */
-const options = {
-  defaultLang: "js",
-  keepBackground: false,
-  theme: {
-    dark: "github-dark-high-contrast",
-    light: "github-light",
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.microlink.io',
+        pathname: '**',
+      },
+    ],
   },
 };
 
-const withMDX = createMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [[rehypePrettyCode, options]],
-  },
-})
-
-// Merge MDX config with Next.js config
-export default withMDX(nextConfig)
+export default nextConfig;
